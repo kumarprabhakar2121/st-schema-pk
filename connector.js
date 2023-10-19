5992561311649710e620f0a99b18979a45d50bdf
Prabhakar Kumar <Prabhakar.Kumar@obloliving.com>
2023-10-19 10:04:04 +0530

    minor

--- connector.js
+++ connector.js
@@ -1,53 +1,67 @@
 const { SchemaConnector, DeviceErrorTypes } = require("st-schema");
 const deviceStates = { switch: "off", level: 100 };
 const connector = new SchemaConnector()
-  .discoveryHandler((accessToken, response) => {
-    response
-      .addDevice("external-device-1", "Test Dimmer", "c2c-dimmer")
-      .manufacturerName("Example Connector")
-      .modelName("Virtual Dimmer");
-  })
-  .stateRefreshHandler((accessToken, response) => {
-    response.addDevice("external-device-1", [
-      {
-        component: "main",
-        capability: "st.switch",
-        attribute: "switch",
-        value: deviceStates.switch,
-      },
-      {
-        component: "main",
-        capability: "st.switchLevel",
-        attribute: "level",
-        value: deviceStates.level,
-      },
-    ]);
-  })
-  .commandHandler((accessToken, response, devices) => {
-    for (const device of devices) {
-      const deviceResponse = response.addDevice(device.externalDeviceId);
-      for (cmd of device.commands) {
-        const state = {
-          component: cmd.component,
-          capability: cmd.capability,
-        };
-        if (cmd.capability === "st.switchLevel" && cmd.command === "setLevel") {
-          state.attribute = "level";
-          state.value = deviceStates.level = cmd.arguments[0];
-          deviceResponse.addState(state);
-        } else if (cmd.capability === "st.switch") {
-          state.attribute = "switch";
-          state.value = deviceStates.switch =
-            cmd.command === "on" ? "on" : "off";
-          deviceResponse.addState(state);
-        } else {
-          deviceResponse.setError(
-            `Command '${cmd.command} of capability '${cmd.capability}' not supported`,
-            DeviceErrorTypes.CAPABILITY_NOT_SUPPORTED
-          );
+    .discoveryHandler((accessToken, response) => {
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:5 ~ .discoveryHandler ~ response:`, response)
+
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:5 ~ .discoveryHandler ~ accessToken:`, accessToken)
+
+        response
+            .addDevice("external-device-1", "Test Dimmer", "c2c-dimmer")
+            .manufacturerName("Example Connector")
+            .modelName("Virtual Dimmer");
+    })
+    .stateRefreshHandler((accessToken, response) => {
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:15 ~ .stateRefreshHandler ~ response:`, response)
+
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:15 ~ .stateRefreshHandler ~ accessToken:`, accessToken)
+
+        response.addDevice("external-device-1", [
+            {
+                component: "main",
+                capability: "st.switch",
+                attribute: "switch",
+                value: deviceStates.switch
+            },
+            {
+                component: "main",
+                capability: "st.switchLevel",
+                attribute: "level",
+                value: deviceStates.level
+            }
+        ]);
+    })
+    .commandHandler((accessToken, response, devices) => {
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:35 ~ .commandHandler ~ devices:`, devices)
+
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:35 ~ .commandHandler ~ response:`, response)
+
+        console.log(`🚀 ${(new Date).toLocaleString()} ~ file: connector.js:35 ~ .commandHandler ~ accessToken:`, accessToken)
+
+        for (const device of devices) {
+            const deviceResponse = response.addDevice(device.externalDeviceId);
+            for (cmd of device.commands) {
+                const state = {
+                    component: cmd.component,
+                    capability: cmd.capability
+                };
+                if (cmd.capability === "st.switchLevel" && cmd.command === "setLevel") {
+                    state.attribute = "level";
+                    state.value = deviceStates.level = cmd.arguments[0];
+                    deviceResponse.addState(state);
+                } else if (cmd.capability === "st.switch") {
+                    state.attribute = "switch";
+                    state.value = deviceStates.switch = cmd.command === "on" ? "on" : "off";
+                    deviceResponse.addState(state);
+                } else {
+                    deviceResponse.setError(
+                        `Command '${cmd.command} of capability '${cmd.capability}' not supported`,
+                        DeviceErrorTypes.CAPABILITY_NOT_SUPPORTED
+                    );
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
 module.exports = connector;
+
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const connector = require("./connector.js");
 const server = express();
-const port = 3000;
+const port = 3003;
 server.use(express.json());
 
 server.post("/", (req, res) => {
-  if (accessTokenIsValid(req)) {
-    connector.handleHttpCallback(req, res);
-  }
+    // if (accessTokenIsValid(req)) {
+        connector.handleHttpCallback(req, res);
+    // }
 });
 
 function accessTokenIsValid(req) {
-  // Replace with proper validation of issued access token
-  if (req.body.authentication.token) {
-    return true;
-  }
-  res.status(401).send("Unauthorized");
-  return false;
+    // Replace with proper validation of issued access token
+    if (req.body.authentication.token) {
+        return true;
+    }
+    res.status(401).send("Unauthorized");
+    return false;
 }
 
 server.listen(port);
 console.log(`Server listening on http://127.0.0.1:${port}`);
+
